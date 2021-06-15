3e451ace76567159a9c7e74e56a54610e812b21f
AdarshChaurasia <68160940+Adarsh-chaurasia@users.noreply.github.com>
2021-06-15 19:11:14 +0100

    Create UglyNumbers.py

---
+++ UglyNumbers.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        if n==1:
+            return True
+        if n==0:
+            return False
+        if n%2==0:
+            return self.isUgly(n//2)
+        if n%3==0:
+            return self.isUgly(n//3)
+        if n%5==0:
+            return self.isUgly(n//5)
+        
+        return 0
