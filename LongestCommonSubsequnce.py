4e4051f7e43e09101d084c8a13722cc6d1b2786c
AdarshChaurasia <68160940+Adarsh-chaurasia@users.noreply.github.com>
2021-07-18 11:16:11 +0100

    Create LongestCommonSubsequnce.py

---
+++ LongestCommonSubsequnce.py
@@ -0,0 +1,20 @@
+def lcs(x,y,n,m):
+    dp=[[0 for j in range(m+1)]for i in range(n+1)]
+    for i in range(1,n+1):
+        for j in range(1,m+1):
+            if x[i-1]==y[j-1]:
+                dp[i][j]=1+dp[i-1][j-1]
+            else:
+                dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+    return dp[n][m]
+
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        n=len(text1)
+        m=len(text2)
+        
+        result=lcs(text1,text2,n,m)
+        
+        
+        return result
